9b7be7c8c74a00dc4c9e086d88b91f0d18ec3444
ariel murphy <arielladders@gmail.com>
2020-07-07 19:35:13 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,8 +1,8 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
-def greeting = " Hello World "
-  puts greeting
+def greeting 
+  puts "Hello World"
 end
 
 greeting
